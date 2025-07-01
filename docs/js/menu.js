f9cac39d89a64554328e343c9400cf8152452bcc
MicahDoo <57170797+MicahDoo@users.noreply.github.com>
2025-07-01 23:54:37 +0800

    Update menu.js

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -16,7 +16,7 @@ function setMenuState(shouldBeOpen) {
         setTimeout(() => mobileMenu.classList.remove('opacity-0', 'pointer-events-none'), 10);
         console.log(`[Menu State] Menu state set to OPEN`);
     } else {
-        mobileMenu.classList.add('hidden')
+        mobileMenu.classList.add('hidden');
         setTimeout(() => mobileMenu.classList.add('opacity-0', 'pointer-events-none');, 300);
         console.log(`[Menu State] Menu state set to CLOSE`);
     }
